47f37d1d5a1f9b4862053c4dd6b19cab23cf79d2
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-01 15:47:36 -0400

    add access to public folder

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 //sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('pubic'));
 
 
 //starts server to begin listening
